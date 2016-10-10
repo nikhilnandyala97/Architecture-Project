9cc9c056f496bfb93a956041070d0d571545ce3e
nikhil nandyala <2015csb1020@iitrpr.ac.in>
2016-10-10 22:38:20 +0530

    Update mySimpleSim.c

--- mySimpleSim.c
+++ mySimpleSim.c
@@ -32,7 +32,10 @@ static unsigned int operand1;
 static unsigned int operand2;
 static int branchPC;
 static int isBranchTaken;
-
+static int isImm;
+static int isADD;
+static int isSUB;
+static int branchTarget;
 void run_simplesim() {
   while(1) {
     fetch();
@@ -89,21 +92,121 @@ void fetch()
                  }
            else{
 		PC=PC+4;
-               }
+               } 
 
           
 	unsigned int *a=read_word(MEM,PC);
 	instruction_word=*a;	
 	
 }
-//reads the instruction register, reads operand1, operand2 fromo register file, decides the operation to be performed in execute stage
 void decode() 
-{
+{int e;
+if(isRet){
+
+operand1=r[15];
+
+}
+else{
+
+e=((instruction_word & 0x003c0000)>>18);
+
+operand1=r[e];
+}
+if(isSt){
+
+e=((instruction_word & 0x03c00000)>>22);
+operand2=r[e];
+}
+else{
+
+e=((instruction_word & 0x0003c000)>>14);
+
+operand2=r[e];
+}
+ if(isImm){
+ if(((instruction_word & 0x00030000)>>16)==0){
+ imm=(instruction_word & 0x0000ffff);
+	 
+ 
+ }
+ if(((instruction_word & 0x00030000)>>16)==1){
+ imm=(instruction_word | 0xffff0000);
+	 
+ 
+ }
+ if(((instruction_word & 0x00030000)>>16)==2){
+ imm=(instruction_word)<<16;
+	 
+ 
+ }
+ 
+ }
+ if(isBranchTaken){
+ 
+ 
+ 
+ }
+ 
+ 
+ 
 }
 //executes the ALU operation based on ALUop
-void execute() {
-}
+
+ 
+ 
+ 
 //perform the memory operation
+rand1=r[e];
+}
+if(isSt){
+
+e=((instruction_word & 0x000001e0)>>5);
+operand2=r[e];
+}
+void execute() 
+{
+	unsigned int a=instruction_word;
+	if(a>>31==1)
+	{	
+		int x=a>>27;
+		int first=(x==18);  /*b */
+		int second=(x==16); /* beq*/
+		int third=(x==17);  /* bgt */
+		int second=(second)&&(eq);
+		int third=(third)&&(gt);
+		int fourth=(x==19); /*call*/
+		int fifth=(x==20)   /*return*/
+		if(first||second||third||fourth||fifth)
+		{
+			isBranchTaken=1;
+		}	
+		else
+		{
+			isBranchTaken=0;
+		}
+	}
+	else
+	{
+	 isBranchTaken=0;
+	}
+
+
+	if(isRet)      /*Updating Branch PC*/
+	branchPC=branchTarget;
+	else
+	branchPC=operand1;
+	
+	/*ALU STARTS HERE*/
+	int A=operand1
+	int B;
+	if(isImm)
+	   B=immx;
+	else
+	   B=operand2;
+	   
+	
+}
+
 void mem() {
 }
 //writes the results back to register file
